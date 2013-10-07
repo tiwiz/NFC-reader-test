c3d99e2ee9a7aba92204652214ed02b7bce6938f
tiwiz <roberto.orgiu@gmail.com>
2013-10-07 21:50:40 +0200

    NFC correctly scanned from within Activity

--- src/mobile/tiwiz/NFC_Reader_Test/NFCActivity.java
+++ src/mobile/tiwiz/NFC_Reader_Test/NFCActivity.java
@@ -4,10 +4,20 @@ import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.nfc.FormatException;
+import android.nfc.NdefMessage;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.Log;
 import android.widget.TextView;
+import org.ndeftools.Message;
+import org.ndeftools.Record;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.util.List;
 
 public class NFCActivity extends Activity {
     /**
@@ -62,7 +72,28 @@ public class NFCActivity extends Activity {
     public void onNewIntent(Intent intent) { // this method is called when an NFC tag is scanned
 
         if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())) {
-            txtResults.setText("TAG Rilevato\n***********************");
+            //takes NDEF Messages received in input
+            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if(null != rawMsgs){
+                NdefMessage message = (NdefMessage) rawMsgs[0];
+                byte[] payload = message.getRecords()[0].getPayload();
+
+                String strPayload = new String(payload, Charset.forName("UTF-8"));
+                if(strPayload.contains("arteatesina.it")){
+                    String output = "";
+                    String[] temp = strPayload.split("=");
+                    //checks if id has been provided
+                    if(temp.length>1){
+                        String weddingID = temp[1];
+                        output += ("***********************\nFormato valido\n***********************");
+                        output += ("ID: " + weddingID);
+                    }else{
+                        output += ("***********************\nFormato NON valido\n***********************");
+                    }
+                    Log.d("TAG_TEST","Output:\n\n" + output);
+                }else
+                    Log.d("TAG_TEST","NO!");
+            }
         } else {
             // ignore
         }
